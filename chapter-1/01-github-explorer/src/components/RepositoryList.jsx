238fe39a408bb42b88cc6763a5d61aa06e3d3caf
Germano Lira <dolan.3@hotmail.com>
2021-05-06 19:16:47 -0300

    consumindo a api do github

--- chapter-1/01-github-explorer/src/components/RepositoryList.jsx
+++ chapter-1/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,23 +1,27 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Fomrs in React',
-    link: 'https://github.com/unform/unform'
-}
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []) // Cuidado para não deixar sem o ultimo parametro
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
