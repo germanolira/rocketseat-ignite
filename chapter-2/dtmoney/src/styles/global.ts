1eca5fcdf1d1cbd298f3d0325b8a1d8f4a6b213a
Germano Lira <dolan.3@hotmail.com>
2021-05-10 15:07:08 -0300

    anotações no código
    anotações no código css para lembrar sobre acessibilidade usando a tag rem no lugar de px

--- chapter-2/dtmoney/src/styles/global.ts
+++ chapter-2/dtmoney/src/styles/global.ts
@@ -13,6 +13,11 @@ export const GlobalStyle = createGlobalStyle`
 
     // Font-size: 16px (Desktop) -> vem por padrão
     // Use REM como padrão do projeto, principalmente para responsividade
+    // Principalmente para acessibilidade, caso o usuario use por padrão
+    // Do celular, uma fonte maior ou menor do que o padrão, usando a tag
+    // REM, ela vai se adaptar para o padrão do celular do usuario, e não
+    // Utilizar um tamanho de fonte fixo, que é no caso o que acontece
+    // Com a tag px no css
 
     html {
         @media (max-width: 1080px) {
