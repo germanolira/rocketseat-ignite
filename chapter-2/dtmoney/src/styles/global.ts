53984249091efeaa60b57bb7879402eecdcfe0aa
Germano Lira <dolan.3@hotmail.com>
2021-05-10 16:11:08 -0300

    adicionando as cores root e o header
    criando as cores root e criando o header e estilizando com styled-components

--- chapter-2/dtmoney/src/styles/global.ts
+++ chapter-2/dtmoney/src/styles/global.ts
@@ -3,6 +3,12 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        --blue-light: #6933FF;
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+        --shape: #FFFFFF;
     }
 
     * {
@@ -29,8 +35,26 @@ export const GlobalStyle = createGlobalStyle`
         }
     }
 
-    body{
+    body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disable] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `
