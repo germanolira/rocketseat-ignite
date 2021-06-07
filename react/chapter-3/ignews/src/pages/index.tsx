06a4c837bad9f726c28f2b97c499bc4324040cd8
Germano Lira <dolan.3@hotmail.com>
2021-06-06 21:05:50 -0300

    Next Auth com Stripe

--- react/chapter-3/ignews/src/pages/index.tsx
+++ react/chapter-3/ignews/src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Post do blog
+
+// Conteudo (SSG)
+// Comentários (Client-side)
 
 interface HomeProps {
   product: {
@@ -37,7 +46,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1It38MLuvglo7AbRNo9bvuqT')
 
   const product = {
@@ -51,6 +60,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
