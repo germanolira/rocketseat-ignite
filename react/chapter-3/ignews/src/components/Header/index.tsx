8f4dcb8d356d1d587d07e08d4995da0e971eca6c
Germano Lira <dolan.3@hotmail.com>
2021-06-15 14:53:50 -0300

    Commit

--- react/chapter-3/ignews/src/components/Header/index.tsx
+++ react/chapter-3/ignews/src/components/Header/index.tsx
@@ -1,14 +1,29 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
 import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss'; 
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+    const { asPath } = useRouter()
+
+    console.log(    )
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink href="/post" prefetch activeClassName={styles.active}>
+                        <a>Posts</a>
+                    </ActiveLink>
+
                 </nav>
 
                 <SignInButton />
